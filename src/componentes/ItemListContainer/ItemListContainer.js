cc9a8de579fe843489893f86bbcab5bf1a214ee3
Ignacio Allue <ignacioallue@hotmail.com>
2023-03-23 04:58:23 -0300

    tienda

--- src/componentes/ItemListContainer/ItemListContainer.js
+++ src/componentes/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,48 @@
+import { useEffect, useState } from 'react'
 import './ItemListContainer.css'
+import MOCK_DATA from '../../data/MOCK_DATA.json'
 
-const ItemListContainer = ( { greeting} ) => {
-    return(
-         <div className='container'>
-              <h2 className='saludo'>Bienvenido</h2>
-              <hr/>
-             <p>{greeting}</p>
-         </div>
+
+const PedirDatos = () =>{
+    return new Promise((resolve,reject) => {
+        setTimeout(() =>{
+            resolve(MOCK_DATA)
+        }, 1000)    
+    })
+} 
+
+const ItemListContainer = () => {
+
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        PedirDatos()
+            .then((res) => {
+                setProductos(res)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
+    return (
+        <div className="container my-5">
+            <h2 className="list-container__title">ItemListContainer</h2>
+            <hr/>
+            <div className='row'>
+            {productos.map((prod) => (
+            <div className='col-3 m-3'>
+                <h2>{prod.name}</h2>
+                <img src={prod.img}/>
+                <p>{prod.description}</p>
+                <p>Precio: ${prod.price}</p>
+                <p>{prod.category}</p>
+                <button>Ver más</button>
+            </div>))
+            }
+            </div>
+           
+        </div>
     )
 }
 
