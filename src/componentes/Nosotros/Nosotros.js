d942f0e429f506dfac61fdd252d8d127d32c79ff
Ignacio Allue <ignacioallue@hotmail.com>
2023-05-08 00:43:54 -0300

    update final

--- src/componentes/Nosotros/Nosotros.js
+++ src/componentes/Nosotros/Nosotros.js
@@ -5,7 +5,7 @@ const Nosotros = () => {
         <div className='container my-5'>
             <h2 className="list-container__title">Nosotros</h2>
             <hr/>
-            <p>Buenos dias, nuestra pagina aún esta en desarrollo, vuelve mas tarde para ver el resultado.</p>
+            <p className='informacion'>Buenos dias, nuestra pagina esta diseñada para brindar productos acerca del camping a todos los apasionados de este hobby, es libre de visitar y checkear nuestros productos para añadir mas comodidad a su proximo viaje</p>
         </div>   
         )
 }
